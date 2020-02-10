e98f9eee7e6abb3206b0461fa705a424f0befba9
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-02-10 14:21:16 -0500

    Made Default Screens and hooked up Initial Navigator

---
+++ src/screens/TrackCreateScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const TrackCreateScreen = () => {
+  return <Text style={{fontSize:48}}>TrackCreateScreen</Text>
+}
+const styles= StyleSheet.create({});
+
+export default TrackCreateScreen;
