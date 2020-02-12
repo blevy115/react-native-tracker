faf3a654abd096da9e83d061d7234baeb13fc2e3
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-02-11 20:45:07 -0500

    Added axios ngrok and base url connecting to track-server api, also started signup function

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,44 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
   switch (action.type){
+    case 'add_error':
+      return { ...state, errorMessage:action.payload };
     default:
       return state;
   }
 };
 
+const signup = dispatch => {
+  return async ({email, password}) => {
+    try {
+      const response = await trackerApi.post('/signup', { email, password })
+      console.log(response.data);
+    } catch (err) {
+      dispatch({ type: 'add_error', payload: 'Something went wrong with Sign Up'})
+      console.log(err.response.data);
+    }
+  }
+}
+
+const signin = dispatch => {
+  return ({email, password}) => {
+    // try to sign in
+    // success change state
+    // fail send error message
+  }
+}
+
+const signout = dispatch => {
+  return () => {
+
+  }
+}
+
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
-  { isSignedIn: false }
+  { signup, signin, signout },
+  { authToken: null, errorMessage: '' }
 );
