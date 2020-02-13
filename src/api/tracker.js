54045d9ec8549cfb63e88e7926be88569dc2802e
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-02-13 12:45:53 -0500

    Extracted form Logic into resuable components and built Signin Screen

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://4fefda42.ngrok.io' // need to change every 8 hours
+  baseURL: 'http://e24fea1c.ngrok.io' // need to change every 8 hours
 });
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,9 +1,41 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Context as AuthContext} from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
-const SigninScreen = () => {
-  return <Text style={{fontSize:48}}>SigninScreen</Text>
-}
-const styles= StyleSheet.create({});
+const SigninScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
+
+  return (
+    <View style={styles.container}>
+    <AuthForm
+      headerText = "Sign In to Tracker"
+      errorMessage = {state.errorMessage}
+      submitText = "Sign In"
+      onSubmit = {signup}
+    />
+    <NavLink
+      navText="Don't have an account? Sign Up instead"
+      routeName="Signup"
+    />
+  </View>
+)};
+
+SigninScreen.navigationOptions = {
+    headerShown:false
+};
+
+
+const styles= StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent:'center',
+    marginBottom:180
+  },
+  link: {
+    color:'blue'
+  }
+});
 
 export default SigninScreen;
