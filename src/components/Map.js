56772fd6731e3ef5b2cb9c3db3c60800546eb77d
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-02-17 20:04:49 -0500

    Added test Polyline

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Polyline } from 'react-native-maps';
 import Spacer from './Spacer'
 
 const Map = () => {
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: 37.33233 + i * 0.001,
+        longitude: -122.03121 + i * 0.001,
+      })
+    } else {
+      points.push({
+        latitude: 37.33233 - i * 0.002,
+        longitude: -122.03121 + i * 0.001,
+      })
+    }
+  }
+
+
   return (
     <Spacer>
       <MapView
@@ -14,7 +30,9 @@ const Map = () => {
           latitudeDelta: 0.01,
           longitudeDelta: 0.01
         }}
-      />
+      >
+        <Polyline coordinates={points}/>
+      </MapView>
     </Spacer>
   )
 }
