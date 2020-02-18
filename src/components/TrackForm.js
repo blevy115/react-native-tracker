6de388cc2fe8081b59434a9febcff0ca5f42d618
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-02-18 16:45:58 -0500

    Built Save Track Hook which grabs name and loctions from recording

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,11 +2,12 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 
 const TrackForm = () => {
   const { state: { name, recording, locations }, startRecording, stopRecording, changeName } = useContext(LocationContext);
-
+  const [saveTrack] = useSaveTrack();
   return <>
     <Spacer>
     <Input value={name} onChangeText={changeName} placeholder="Enter Name"/>
@@ -20,10 +21,10 @@ const TrackForm = () => {
     </Spacer>
     <Spacer>
     { !recording && locations.length ? (
-          <Button title="Save Recording" />
+          <Button title="Save Recording" onPress={saveTrack}/>
       ) : null
     }
-    </Spacer>  
+    </Spacer>
   </>
 };
 
